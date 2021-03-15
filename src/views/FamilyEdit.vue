e0054d4d0da69974efe826fe2b816ad4e0038a5b
Davis Brown <davis.brown@eagles.oc.edu>
2021-03-15 14:11:41 -0500

    update family add and edit

--- src/views/FamilyEdit.vue
+++ src/views/FamilyEdit.vue
@@ -1,71 +1,118 @@
 <template>
-  <div v-if="currentFamily" class="edit-form py-3">
+  <div v-if="currentFamily" class="edit-form">
       <v-form ref="form" lazy-validation>
-
+        
       <v-text-field
         v-model="currentFamily.fam_name"
         :rules="[(v) => !!v || 'First name is required']"
         label="Family Name"
         required
       ></v-text-field>
-
-      <v-select :items="familyPersons"
-        label="Head of the Family"
-        item-text ="person.frst_name"
-        item-value= "per_ID" 
-        v-model="currentFamily.per_ID" >
-        <template v-slot:selection="{ item } " >
-          {{item.person.frst_name}} {{item.person.last_name}}
-        </template>
-      </v-select>
-
-      <v-data-table
-        :headers="headers"
-        :items="familyPersons"
-        hide-default-footer
-        class="elevation - 1">
-        <template v-slot:item="{ item }">
-            <v-icon
-              small
-              class="mr-2"
-              @click="deletePersonForFamily(item)"
+      
+      <v-card>
+        <v-data-table
+          :headers="headers"
+          :items="familyPersons"
+          hide-default-footer
+          >
+          <template v-slot:top>
+            <v-toolbar
+              flat
             >
-              mdi-delete
-            </v-icon>
-        </template>
-      </v-data-table>
-
-      <v-row justify="center" align="center">
-        <v-col justify="left" col="1"> 
-            <v-autocomplete
-                v-model="familyPerson.per_ID"
-                :items="people"
-                label="Family Member"
-                item-value="per_ID"
-                :filter="customFilter"
+              <v-toolbar-title>Family Members</v-toolbar-title>
+              <v-divider
+                class="mx-4"
+                inset
+                vertical
+              ></v-divider>
+              <v-spacer></v-spacer>
+              <v-dialog
+                v-model="dialog"
+                max-width="500px"
               >
-                <template slot="selection" slot-scope="data" >
-                  {{data.item.frst_name}} {{data.item.last_name}}
+                <template v-slot:activator="{ on, attrs }">
+                  <v-btn
+                    color="primary"
+                    dark
+                    class="mb-2"
+                    v-bind="attrs"
+                    v-on="on"
+                  >
+                    Add Member
+                  </v-btn>
                 </template>
-                <template slot="item" slot-scope="data" >
-                  {{data.item.frst_name}} {{data.item.last_name}}
-                </template>
-              </v-autocomplete>
-        </v-col>
-        <v-col justify="left" col="2"> 
-            <v-text-field
-              v-model="familyPerson.fam_role"
-              label="Family Role"
-              required
-            ></v-text-field>
-        </v-col>
-        <v-col align="center" col="3">  
-              <v-btn color="success" @click="addPersonForFamily()" center>
-                Add
-              </v-btn>
-        </v-col>
-      </v-row>
+                <v-card>
+                  <v-card-title>
+                    <span>New Family Member</span>
+                  </v-card-title>
 
+                  <v-card-text>
+                    <v-container>
+                      <v-row justify="center" align="center">
+                        <v-col justify="left" col="1"> 
+                            <v-autocomplete
+                                v-model="familyPerson.per_ID"
+                                :items="people"
+                                label="Family Member"
+                                item-value="per_ID"
+                                :filter="customFilter"
+                              >
+                                <template slot="selection" slot-scope="data" >
+                                  {{data.item.frst_name}} {{data.item.last_name}}
+                                </template>
+                                <template slot="item" slot-scope="data" >
+                                  {{data.item.frst_name}} {{data.item.last_name}}
+                                </template>
+                              </v-autocomplete>
+                        </v-col>
+                        <v-col justify="left" col="2"> 
+                            <v-text-field
+                              v-model="familyPerson.fam_role"
+                              label="Family Role"
+                              required
+                            ></v-text-field>
+                        </v-col>
+                      </v-row>
+                    </v-container>
+                  </v-card-text>
+
+                  <v-card-actions>
+                    <v-spacer></v-spacer>
+                    <v-btn
+                      color="blue darken-1"
+                      text
+                      @click="close"
+                    >
+                      Cancel
+                    </v-btn>
+                    <v-btn
+                      color="blue darken-1"
+                      text
+                      @click="addPersonForFamily()"
+                    >
+                      Save
+                    </v-btn>
+                  </v-card-actions>
+                </v-card>
+              </v-dialog>
+            </v-toolbar>
+          </template>
+          <template v-slot:item.head="{ item }">
+            <v-radio-group
+              v-model="currentFamily.per_ID"
+              name="rowSelector">
+              <v-radio :value="item.person.per_ID"/>
+            </v-radio-group>
+          </template>
+          <template v-slot:item.actions="{ item }">
+              <v-icon
+                @click="deletePersonForFamily(item)"
+              >
+                mdi-delete
+              </v-icon>
+          </template>
+        </v-data-table>
+      </v-card>
       <v-divider class="my-5"></v-divider>
 
       <v-row justify="center">
@@ -99,6 +146,7 @@ import FamilyPersonService from "../services/FamilyPersonService";
 export default {
   data() {
     return {
+      dialog: false,
       currentFamily: null,
       familyPersons: [],
       people: [],
@@ -106,19 +154,24 @@ export default {
       message: '',
       headers: [
                 {
-                    text: 'First Name',
+                    text: 'Name',
                     align: 'left',
                     value: 'person.frst_name',
                 },
                 {
-                    text: 'Last Name',
+                    text: '',
                     align: 'left',
                     value: 'person.last_name',
                 },
                 {
-                    text: 'Family Role',
+                    text: 'Role',
                     align: 'left',
                     value: 'fam_role',
+                },
+                {
+                    text: 'Family Head',
+                    align: 'left',
+                    value: 'head',
                 },
                 {
                     text: 'Action',
@@ -198,6 +251,7 @@ export default {
     },
 
     addPersonForFamily() {
+        this.dialog = false
         let familyPerson = {};
         familyPerson.fam_ID = this.currentFamily.fam_ID;
         familyPerson.per_ID = this.familyPerson.per_ID;
@@ -213,6 +267,9 @@ export default {
           });
            
     },
+    close () {
+        this.dialog = false
+      },
 
     customFilter (item, queryText) {
       const textOne = item.frst_name.toLowerCase()
@@ -238,9 +295,7 @@ h4 {
   text-align: center;
 }
 .edit-form {
-
-  max-width: 600px;
-
+  max-width: 600;
   margin: auto;
 }
 </style>
