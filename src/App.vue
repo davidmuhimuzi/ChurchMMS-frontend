7b38f372afd275238d7a4f298c164e9f9c60ea1e
David Muhimuzi <davidmuhimuzi35@gmail.com>
2021-03-24 14:00:17 -0500

    Fixed the color issue (added v-app)

--- src/App.vue
+++ src/App.vue
@@ -1,86 +1,87 @@
 <template>
- <div id="app">
-   <header class="header header-absolute bg-dark" style="height: 150px">
-     <div class="container-fluid">
-        <img style="padding-top:20px; display:block; margin-left:auto; margin-right:auto;" src="./assets/logowilshire.png">
-        
-      </div>
-   </header>
-    <nav class="navbar navbar-expand  navbar-dark bg-dark">
-      <a href class="navbar-brand" @click.prevent>Wilshire Church Membership System</a>
-      <div class="navbar-nav mr-auto ml-auto">
-        <li class="nav-item">
-          <router-link to="/home" class="nav-link">
-            Home
-          </router-link>
-        </li>
-        
-        <li v-if="currentUser" class="nav-item">
-          <router-link to="/congregation" class="nav-link">
-          Congregation
-          </router-link>
-        </li>
-         <li v-if="currentUser" class="nav-item">
-          <router-link to="/calendar" class="nav-link">
-          Events Calendar
-          </router-link>
-        </li>
-         <li v-if="currentUser" class="nav-item">
-          <router-link to="/person" class="nav-link">
-          Members
-          </router-link>
-        </li>
-         <li v-if="currentUser" class="nav-item">
-          <router-link to="/families" class="nav-link">
-          Families
-          </router-link>
-        </li>
-      </div>
+<v-app>
+  <div id="app">
+    <header class="header header-absolute bg-dark" style="height: 150px">
+      <div class="container-fluid">
+          <img style="padding-top:20px; display:block; margin-left:auto; margin-right:auto;" src="./assets/logowilshire.png">
+          
+        </div>
+    </header>
+      <nav class="navbar navbar-expand  navbar-dark bg-dark">
+        <a href class="navbar-brand" @click.prevent>Wilshire Church Membership System</a>
+        <div class="navbar-nav mr-auto ml-auto">
+          <li class="nav-item">
+            <router-link to="/home" class="nav-link">
+              Home
+            </router-link>
+          </li>
+          
+          <li v-if="currentUser" class="nav-item">
+            <router-link to="/congregation" class="nav-link">
+            Congregation
+            </router-link>
+          </li>
+          <li v-if="currentUser" class="nav-item">
+            <router-link to="/calendar" class="nav-link">
+            Events Calendar
+            </router-link>
+          </li>
+          <li v-if="currentUser" class="nav-item">
+            <router-link to="/person" class="nav-link">
+            Members
+            </router-link>
+          </li>
+          <li v-if="currentUser" class="nav-item">
+            <router-link to="/families" class="nav-link">
+            Families
+            </router-link>
+          </li>
+        </div>
 
-      <div v-if="!currentUser" class="navbar-nav ml-auto">
-        <li class="nav-item">
-          <router-link to="/register" class="nav-link">
-            <font-awesome-icon icon="user-plus" />Sign Up
-          </router-link>
-        </li>
-        <li class="nav-item">
-          <router-link to="/login" class="nav-link">
-            <font-awesome-icon icon="sign-in-alt" />Login
-          </router-link>
-        </li>
-      </div>
+        <div v-if="!currentUser" class="navbar-nav ml-auto">
+          <li class="nav-item">
+            <router-link to="/register" class="nav-link">
+              <font-awesome-icon icon="user-plus" />Sign Up
+            </router-link>
+          </li>
+          <li class="nav-item">
+            <router-link to="/login" class="nav-link">
+              <font-awesome-icon icon="sign-in-alt" />Login
+            </router-link>
+          </li>
+        </div>
 
-      <div v-if="currentUser" class="navbar-nav ml-auto">
-        <li v-if="showAdminBoard" class="nav-item">
-          <router-link to="/admin" class="nav-link">Admin Board</router-link>
-        </li>
-        <li v-if="showModeratorBoard" class="nav-item">
-          <router-link to="/mod" class="nav-link">Moderator Board</router-link>
-        </li>
-        <li class="nav-item">
-          <router-link v-if="currentUser" to="/user" class="nav-link">User</router-link>
-        </li>
+        <div v-if="currentUser" class="navbar-nav ml-auto">
+          <li v-if="showAdminBoard" class="nav-item">
+            <router-link to="/admin" class="nav-link">Admin Board</router-link>
+          </li>
+          <li v-if="showModeratorBoard" class="nav-item">
+            <router-link to="/mod" class="nav-link">Moderator Board</router-link>
+          </li>
+          <li class="nav-item">
+            <router-link v-if="currentUser" to="/user" class="nav-link">User</router-link>
+          </li>
 
-        <li class="nav-item">
-          <router-link to="/profile" class="nav-link">
-            <font-awesome-icon icon="user" />
-            {{ currentUser.username }}
-          </router-link>
-        </li>
-        <li class="nav-item">
-          <a class="nav-link" href @click.prevent="logOut">
-            <font-awesome-icon icon="sign-out-alt" />LogOut
-          </a>
-        </li>
-      </div>
-    </nav>
- 
+          <li class="nav-item">
+            <router-link to="/profile" class="nav-link">
+              <font-awesome-icon icon="user" />
+              {{ currentUser.username }}
+            </router-link>
+          </li>
+          <li class="nav-item">
+            <a class="nav-link" href @click.prevent="logOut">
+              <font-awesome-icon icon="sign-out-alt" />LogOut
+            </a>
+          </li>
+        </div>
+      </nav>
+  
 
-    <v-main>
-      <router-view />
-    </v-main>
-  </div>
-
+      <v-main>
+        <router-view />
+      </v-main>
+    </div>
+</v-app>
 
 
 
